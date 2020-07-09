1a1ef3705ca872b40870b7ec00daecf7cfb40ed4
김준호 <enddl0121@gmail.com>
2020-07-09 19:37:41 +0900

    server 모듈 안됌

---
+++ client/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({ //프론트엔드 포트번호 3000번에서 5000으로 주겠다
+      target: 'http://localhost:5000',
+      changeOrigin: true,
+    })
+  );
+};
