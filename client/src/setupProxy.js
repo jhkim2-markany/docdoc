ea9e26148d91155761446f549d9bf1943d46473a
김준호 <enddl0121@gmail.com>
2020-07-13 19:47:16 +0900

    compose 수정 해야함

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({ //프론트엔드 포트번호 3000번에서 5000으로 주겠다
-      target: 'http://localhost:5000',
+      target: 'http://192.168.99.100:5000',
       changeOrigin: true,
     })
   );
